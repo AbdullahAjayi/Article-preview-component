bbfb10ce5b63f4adfa4c6814efbc7a982af8f9f3
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-05-27 17:57:04 -0700

    added hover effect on desktop share button with javascript

--- script.js
+++ script.js
@@ -9,8 +9,26 @@ const articleFooter  = document.querySelector('.article-footer');
 function onClick() {
     mobileShareBar.classList.remove('closed');
     articleFooter.classList.add('closed')
-    desktopShareBar.classList.toggle('closed')
+    // desktopShareBar.classList.toggle('closed')
+}
+
+function onHover(){
+    desktopShareBar.classList.remove('closed');
+    desktopShareBar.addEventListener('mouseover', onShareBarHover);
+    function onShareBarHover(){
+    desktopShareBar.classList.remove('closed');
+    }
+}
+
+function onMouseLeave() {
+    desktopShareBar.classList.add('closed');
+    desktopShareBar.addEventListener('mouseleave', onShareBarLeave);
+    function onShareBarLeave(){
+    desktopShareBar.classList.add('closed');
+    }
 }
 
 
 shareBtn.addEventListener('click', onClick);
+shareBtn.addEventListener('mouseenter', onHover);
+shareBtn.addEventListener('mouseleave', onMouseLeave);
